09993526aca7587546825cf80cd31d734aa0e40c
johny <69754601+jskjw157@users.noreply.github.com>
2022-07-17 08:44:59 +0900

    pug 템플릿 엔진 적용, 기본 레이아웃 구조 생성

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,42 @@
-export const trending = (req, res) => res.send("Home Page Videos");
+const fakeUser = {
+    username: "Jiwon",
+    loggedIn: true,
+};
+
+export const trending = (req, res) =>{
+    const videos = [
+        {
+            title: "First Video",
+            rating:5,
+            comments:2,
+            createdAt:"2 minutes ago:",
+            view:59,
+            id: 1,
+        },
+        {
+            title: "Secon Video",
+            rating:5,
+            comments:2,
+            createdAt:"2 minutes ago:",
+            view:59,
+            id: 1,
+        },
+        {
+            title: "Third Video",
+            rating:5,
+            comments:2,
+            createdAt:"2 minutes ago:",
+            view:59,
+            id: 1,
+        }
+    ];
+    res.render("home", {pageTitle: "Home", fakeUser,  videos});
+} 
+    
 export const see = (req, res) => {
-    return res.send(`Watch Video #${req.params.id}`);
+    res.render("watch");
 }
-export const edit = (req,res) => res.send("Edit");
+export const edit = (req,res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("upload");
 export const deleteVideo = (req, res) => res.send("deleteVideo");
